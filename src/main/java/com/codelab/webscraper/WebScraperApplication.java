56b3de6d6ba5bc22626170d8575a96d28d495ec8
Rurangwa <chrisruu@yahoo.com>
2023-09-23 20:27:29 +0800

    updates

--- src/main/java/com/codelab/webscraper/WebScraperApplication.java
+++ src/main/java/com/codelab/webscraper/WebScraperApplication.java
@@ -16,19 +16,19 @@ public class WebScraperApplication {
 		SpringApplication.run(WebScraperApplication.class, args);
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(Boolean.TRUE);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.setAllowCredentials(Boolean.TRUE);
+//		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 //		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 //				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 //				"Access-Control-Request-Method", "Access-Control-Request-Headers","id"));
 //		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
 //				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+//		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//		return new CorsFilter(urlBasedCorsConfigurationSource);
+//	}
 }
--- src/main/java/com/codelab/webscraper/controller/WebScraperController.java
+++ src/main/java/com/codelab/webscraper/controller/WebScraperController.java
@@ -5,12 +5,14 @@ import com.codelab.webscraper.services.ScraperService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class WebScraperController {
 
     private ScraperService scraperServ;
