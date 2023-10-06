97dcb6301153b70b0b1c66e194e53b4601d61a57
Rurangwa <chrisruu@yahoo.com>
2023-10-06 21:32:23 +0800

    updates

--- src/main/java/com/codelab/webscraper/controller/HealthCheckController.java
+++ src/main/java/com/codelab/webscraper/controller/HealthCheckController.java
@@ -3,11 +3,9 @@ package com.codelab.webscraper.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.core.Response;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
